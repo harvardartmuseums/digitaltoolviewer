665a0448f0113820655994ed0a09f895f8042d00
glevyhas <glevyhas@gmail.com>
2017-05-12 14:10:34 -0400

    added all files

---
+++ index.js
@@ -0,0 +1,44 @@
+﻿'use strict';
+
+const app = require('express')();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+var tour = "<!DOCTYPE html><html><head><title>Digital Tool Lightbox Viewer</title></head><body>";
+
+app.get('/index.html', function(req, res){
+	res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/js/three.min.js', function(req, res){
+	res.sendFile(path.join(__dirname, '/js/three.min.js'));
+});
+
+app.get('/js/Mirror.js', function(req, res){
+	res.sendFile(path.join(__dirname, '/js/Mirror.js'));
+});
+
+app.get('/js/CSS3DRenderer.js', function(req, res){
+	res.sendFile(path.join(__dirname, '/js/CSS3DRenderer.js'));
+});
+
+io.on('connection', function(socket) {
+	socket.on("getTour", function(number) {
+		http.get('http://www.harvardartmuseums.org/tour/' + number + '/getInfo', (res) => {
+			if (res.statusCode == 200) {
+				var rawData = '';
+				res.setEncoding('utf8');
+				res.on('data', (data) => {rawData += data;});
+				res.on('end', () => {
+					socket.emit("tourData", JSON.parse(rawData));
+				});
+			} 
+		});
+	});
+});
+
+server.listen(PORT);
