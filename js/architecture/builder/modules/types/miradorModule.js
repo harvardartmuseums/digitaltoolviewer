67850e5dd7aa18a2ab40a787c033fa7eb5049704
glevyhas <glevyhas@gmail.com>
2017-06-12 12:56:05 -0400

    tested removing mirador

--- js/architecture/builder/modules/types/miradorModule.js
+++ js/architecture/builder/modules/types/miradorModule.js
@@ -11,11 +11,11 @@ function MiradorModule(width, height, module) {
 	miradorElement.style.width = 4*width  + "px";
 	miradorElement.style.height = 4*height  + "px";
 
-	miradorElement.innerHTML = "<iframe src=\"http://www.harvardartmuseums.org/miradorviewer/module/" + module.mirador.id + "\" frameborder=\"0\" style=\"border:0\" width=\"" + width*4 + "px\" height=\"" + height*4 + "px\" allowfullscreen></iframe>";
+	//miradorElement.innerHTML = "<iframe src=\"http://www.harvardartmuseums.org/miradorviewer/module/" + module.mirador.id + "\" frameborder=\"0\" style=\"border:0\" width=\"" + width*4 + "px\" height=\"" + height*4 + "px\" allowfullscreen></iframe>";
 
-	setTimeout(function() {
-		generateCutout(this, miradorElement, undefined, undefined, undefined, undefined, true, true, openMirador.bind(module.mirador.id)); 
-	}.bind(this.mesh), 10);
+	//setTimeout(function() {
+	//	generateCutout(this, miradorElement, undefined, undefined, undefined, undefined, true, true, openMirador.bind(module.mirador.id)); 
+	//}.bind(this.mesh), 10);
 	
 	return this.mesh;
 }
