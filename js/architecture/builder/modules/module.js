1c8fac68fe8ad5bb3f5857f90c6c670b130ba9d8
glevyhas <glevyhas@gmail.com>
2017-06-26 11:06:37 -0400

    updated testing lines

--- js/architecture/builder/modules/module.js
+++ js/architecture/builder/modules/module.js
@@ -201,11 +201,13 @@ function generateCutout(mesh, cssElement, pos, width, height, scale, interactive
 	var p = new THREE.Vector3();
 	var r = new THREE.Quaternion();
 	var s = new THREE.Vector3();
+	var p2 = new THREE.Vector3();
+	var r2 = new THREE.Quaternion();
+	var s2 = new THREE.Vector3();
 	cutoutPlane.matrixWorld.decompose(p, r, s);
-	console.log(p.x, p.y, p.z, r.x, r.y, r.z, r.w, s.x, s.y, s.z);
 	cssObject.applyMatrix(cutoutPlane.matrixWorld);
 	cssObject.matrixWorld.decompose(p, r, s);
-	console.log(p.x, p.y, p.z, r.x, r.y, r.z, r.w, s.x, s.y, s.z);
+	console.log("position", p.x - p2.x, p.y - p2.y, p.z, p2.z, "rotation", r.x - r2.x, r.y - r2.y, r.z - r2.z, r.w - r2.w, "scale", s.x, s2.x);
 
 	if (highres) {
 		cssObject.scale.x *= .25;
