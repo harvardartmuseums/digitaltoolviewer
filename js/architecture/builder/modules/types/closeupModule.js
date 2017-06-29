fd4e64716dcb0bc6fe4379dc540a3bf6ec0d8c6f
glevyhas <glevyhas@gmail.com>
2017-06-29 14:59:37 -0400

    adjusted closeup zoom

--- js/architecture/builder/modules/types/closeupModule.js
+++ js/architecture/builder/modules/types/closeupModule.js
@@ -52,7 +52,7 @@ function CloseupModule(width, height, module) {
 			keyframes += "100%, ";
 		}
 		keyframes += (i + .4)*100/module.images[0].hotspots.length + "% ";
-		keyframes += "{left: " + -10*(module.images[0].hotspots[i].coord_x + 5)*(.5 + scaleX/2) + "%; top: " + -10*(module.images[0].hotspots[i].coord_y + 5)*(.5 + scaleY/2) + "%; transform: scale(1, 1)} ";
+		keyframes += "{left: " + -10*(module.images[0].hotspots[i].coord_x + 5)*scaleX + "%; top: " + -10*(module.images[0].hotspots[i].coord_y + 5)*scaleY + "%; transform: scale(1, 1)} ";
 
 		keyframes += (i + .5)*100/module.images[0].hotspots.length + "%, ";
 		keyframes += (i + .9)*100/module.images[0].hotspots.length + "% ";
