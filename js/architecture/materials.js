82d89dda51f4023e73f0b0930540bbd677c92a3c
glevyhas <glevyhas@gmail.com>
2017-07-07 11:14:49 -0400

    adjusted materials

--- js/architecture/materials.js
+++ js/architecture/materials.js
@@ -1,12 +1,12 @@
-var wallMaterial = floorMaterial = ceilingMaterial = new THREE.MeshStandardMaterial({metalness: 0, roughness: .8});
+var wallMaterial = floorMaterial = ceilingMaterial = new THREE.MeshLambertMaterial();
 
-var screenMaterial = new THREE.MeshStandardMaterial({color: 0x000000, metalness: 0, roughness: 0});
-var monitorMaterial = new THREE.MeshStandardMaterial({color: 0x555555, metalness: 0, roughness: .6});
+var screenMaterial = new THREE.MeshPhongMaterial({color: 0x000000});
+var monitorMaterial = new THREE.MeshPhongMaterial({color: 0x555555});
 
-var speakerMaterial = frameMaterial = new THREE.MeshStandardMaterial({color: 0xdddddd, metalness: 0, roughness: .8});
-var speakerGrillMaterial = new THREE.MeshStandardMaterial({color: 0x222222, metalness: 0, roughness: .8});
+var speakerMaterial = frameMaterial = new THREE.MeshLambertMaterial({color: 0xdddddd});
+var speakerGrillMaterial = new THREE.MeshLambertMaterial({color: 0x222222});
 
-var textMaterial = new THREE.MeshStandardMaterial({metalness: 0, color: 0x000000, roughness: .8});
+var textMaterial = new THREE.MeshLambertMaterial({color: 0x000000});
 
 var cutoutMaterial = new THREE.MeshBasicMaterial({color: 0x000000, opacity: 0.0});
 
