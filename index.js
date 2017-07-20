57122469cd29fca7a53be262f23bc34129b2c0a2
glevyhas <glevyhas@gmail.com>
2017-07-20 16:49:23 -0400

    fixed typo

--- index.js
+++ index.js
@@ -123,8 +123,8 @@ controlIO.on('connection', function(socket) {
 				screensIO.to(this).emit("click", e);
 			}.bind(id));
 
-			socket.on("slideOverlay", function(id, this.value) {
-				screensIO.to(this).emit("slideOverlay", id, this.value);
+			socket.on("slideOverlay", function(id, value) {
+				screensIO.to(this).emit("slideOverlay", id, value);
 			}.bind(id));
 		} else {
 			socket.emit("invalid id");
