d9e88f1bb298bd742ff8690d60fe4599438eb9c8
glevyhas <glevyhas@gmail.com>
2017-07-29 12:46:06 -0400

    Revert "added testing line"
    This reverts commit 563e4221d91e023704ae0f0fc6e27a0734fc4878.

--- js/interaction/play/interact.js
+++ js/interaction/play/interact.js
@@ -6,11 +6,6 @@ function setupInteraction() {
 	mouse = new THREE.Vector2();
 
 	socket.on("click", handleInteraction);
-	document.addEventListener("onclick", click);
-}
-
-function click(e) {
-	handleInteraction({x: e.clientX/window.innerWidth*2 - 1, y: e.clientY/window.innerHeight*2 - 1});
 }
 
 function handleInteraction(e) {
